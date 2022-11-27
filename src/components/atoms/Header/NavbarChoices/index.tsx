b7fd6849f309ece6151d4e0527ece10b2892d35b
R.Walerczak <r.walerczak@gmail.com>
2022-11-27 14:03:34 +1100

    Smooth out menu animation, tweak sizing

--- src/components/atoms/Header/NavbarChoices/index.tsx
+++ src/components/atoms/Header/NavbarChoices/index.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 // import { NavbarInterface } from '../../../molecules/HeaderMolecules/Navbar';
 import './NavbarChoices.css';
 
-interface NavbarInterface {
-    name: string,
-    choices: JSX.Element[]
+interface NavbarItemsInterface {
+    Name: string,
+    Link: string,
 }
 
-const NavbarChoices = (props: any) => {
+interface NavbarInterface {
+    name: string,
+    choices: NavbarItemsInterface[]
+}
+
+const NavbarChoices = (props: NavbarInterface) => {
     const [open, setOpen] = React.useState(false);
+    const [dropdownClassName, setDropdownClassName] = React.useState("navbar-choices-dropdown-container-hidden");
+    let navbarChoicesContainer = document.getElementsByClassName('navbar-choices-container').item(0);
+
+    useEffect(() => {
+        navbarChoicesContainer?.classList.toggle('unclickable');
+        setTimeout(() => {
+            navbarChoicesContainer?.classList.toggle('unclickable');
+        }, 400);
+        if(open === true){
+            setDropdownClassName("navbar-choices-dropdown-container");
+            for(let i = 0; i < props.choices.length; i++){
+                setTimeout(() => {
+                    document.getElementsByClassName('navbar-choices-link-hidden').item(0)?.setAttribute('class', 'navbar-choices-link');
+                }, 100*i);
+            };
+        } else {
+            setDropdownClassName("navbar-choices-dropdown-container-hidden");
+            for(let i = 0; i < props.choices.length; i++){
+                setTimeout(() => {
+                    document.getElementsByClassName('navbar-choices-link').item(0)?.setAttribute("class", 'navbar-choices-link-hidden');
+                }, 100*i);
+            };
+        };
+    }, [open]);
+
     return (
-        <div key={0} className="navbar-choices-container" onClick={()=>{setOpen(!open)}}>
+        <div key={0} className="navbar-choices-container">
             <img src={props.name} className="navbar-choices-icon" onClick={()=>{setOpen(!open)}}></img>
-            {open && props.choices.map((choice: any, index: any) => {
+            <div id="dropdown-Container" className={dropdownClassName}>
+            {props.choices.map((choice: any, index: any) => {
                 return (
-                    // <div key={index} className="navbar-choices-link" onClick={()=>{}}>
-                        <NavLink className="navbar-choices-link" key={index} to={`/${choice.Link}`}>{choice.Name}</NavLink>
-                    // </div>
+                    <NavLink onClick={()=>setOpen(!open)}className={"navbar-choices-link-hidden"} key={index} to={`/${choice.Link}`} >{choice.Name}</NavLink>
                 )
             })}
+            </div>
         </div>
     );
 };
--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,17 +1,13 @@
-import { NavLink } from 'react-router-dom';
 import HeaderH1 from '../../atoms/Header/HeaderH1';
 import HeaderH2 from '../../atoms/Header/HeaderH2';
 import NavbarChoices from '../../atoms/Header/NavbarChoices';
-import NavDownloadImage from '../../atoms/NavDownloadImage';
-import NavDownloadLink from '../../atoms/NavDownloadLink';
 import HeaderTextAligner from '../../molecules/HeaderMolecules/HeaderTextAligner';
 // import Navbar from '../../molecules/HeaderMolecules/Navbar';
 import './Header.css'
 
-
 const Header = () => {
     return (
-        <div  className="header">
+        <div className="header">
             <HeaderTextAligner components={[<HeaderH1 componentKey={0} text={"Robert Walerczak"} />, <HeaderH2 componentKey={1} text={"Software Engineer"} />]} />
             <NavbarChoices name={'/images/menuIcon.png'} choices={[{"Name": "Home", "Link": ""}, {"Name": "Projects", "Link": "projects"}, {"Name": "About", "Link": "about"}, 
             {"Name": "Contact", "Link": "contact"}]} />
